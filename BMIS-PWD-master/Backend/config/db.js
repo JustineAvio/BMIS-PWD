095a7e5ac6a27fb5c5245ee02c3951a886f74210
Justine Avio <justinejeckho.avio@cvsu.edu.ph>
2026-06-05 21:23:29 +0800

    adding vercel json file and configuring database connection

--- BMIS-PWD-master/Backend/config/db.js
+++ BMIS-PWD-master/Backend/config/db.js
@@ -3,7 +3,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const URLDB2=`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-const dbconnection = mysql.createPool(URLDB2);
+const dbconnection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 module.exports = dbconnection;
