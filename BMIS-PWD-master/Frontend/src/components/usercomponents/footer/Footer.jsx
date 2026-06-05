b962cb7cdd372ee94db3ef65bbc3da749aae955d
Justine Avio <justinejeckho.avio@cvsu.edu.ph>
2026-06-05 17:00:45 +0800

    fixing the footer and the header

--- BMIS-PWD-master/Frontend/src/components/usercomponents/footer/Footer.jsx
+++ BMIS-PWD-master/Frontend/src/components/usercomponents/footer/Footer.jsx
@@ -1,68 +1,132 @@
+import './Footer.css'
+import { useNavigate, useLocation, Link } from 'react-router-dom'
 
-    import './Footer.css'
+function Footer() {
 
-    function Footer() {
-        return (
-            <footer className="footer">
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleNewsClick = (e) => {
+        e.preventDefault();
+
+        const scrollToNews = () => {
+            const section = document.getElementById("news");
+
+            if (section) {
+                const headerOffset = 100;
+
+                const elementPosition = section.getBoundingClientRect().top;
+                const offsetPosition =
+                    window.pageYOffset + elementPosition - headerOffset;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: "smooth",
+                });
+            }
+        };
+
+        if (location.pathname === "/landing-page") {
+            scrollToNews();
+        } else {
+            navigate("/landing-page");
+
+            setTimeout(() => {
+                scrollToNews();
+            }, 200);
+        }
+    };
+
+    return (
+        <footer className="footer">
             <div className="footercontainer">
 
-            
                 <div className="footercol footerabout">
-                <img src="/images/logo2.png" alt="Barangay Logo" className="logo"/>
+                    <img src="/images/logo2.png" alt="Barangay Logo" className="logo"/>
 
-                <p className="mission">
-                    A Barangay that is God-centered, competent, orderly, honest,
-                    peaceful, credible, gender responsive and abides the Code of Conduct.
-                </p>
+                    <p className="mission">
+                        A Barangay that is God-centered, competent, orderly, honest,
+                        peaceful, credible, gender responsive and abides the Code of Conduct.
+                    </p>
 
-                <p className="versetitle">Philippians 4:13 NKJV</p>
-                <p className="verse">
-                    I can do all things through Christ who strengthens me.
-                </p>
+                    <p className="versetitle">Philippians 4:13 NKJV</p>
+
+                    <p className="verse">
+                        I can do all things through Christ who strengthens me.
+                    </p>
                 </div>
 
-            
                 <div className="footercol">
-                <h3>NAVIGATION</h3>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li><a href="#news">News</a></li>
-                    <li>Certificates</li>
-                    <li>Online Marketplace</li>
-                </ul>
+                    <h3>NAVIGATION</h3>
 
-                <h3 className="contactheading">CONTACT US</h3>
-                <ul>
-                    <li>Facebook Page</li>
-                    <li>Email: BLV@gov.ph</li>
-                </ul>
+                    <ul>
+                        <li onClick={() => navigate('/landing-page')}>Home</li>
+
+                        <li onClick={() => navigate('/aboutus')}>
+                            About Us
+                        </li>
+
+                        <li>
+                            <Link to="#news" onClick={handleNewsClick}>
+                                News
+                            </Link>
+                        </li>
+
+                        <li onClick={() => navigate('/forms')}>
+                            Certificates
+                        </li>
+
+                        <li>
+                            <Link
+                                to="https://www.facebook.com/bayanlumav.imus"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Online Marketplace
+                            </Link>
+                        </li>
+                    </ul>
+
+                    <h3 className="contactheading">CONTACT US</h3>
+
+                    <ul>
+                        <li>
+                            <Link
+                                to="https://www.facebook.com/bayanlumav.imus"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Facebook Page
+                            </Link>
+                        </li>
+
+                        <li>Email: BLV@gov.ph</li>
+                    </ul>
                 </div>
 
-            
                 <div className="footercol">
-                <h3>HOTLINES</h3>
-                <ul className="hotlines">
-                    <li>Imus CDRRMO | (046) 472-2618 / 0939-912-0887</li>
-                    <li>Imus BFP | (046) 416-3032 / 0915-528-3256</li>
-                    <li>Imus PNP | (046) 471-2656</li>
-                    <li>Imus Hospital | (046) 419-8300</li>
-                    <li>Cavite PDRRMO | (046) 419-1652</li>
-                    <li>National Emergency | 911</li>
-                    <li>Red Cross PH | 143</li>
-                    <li>NDRRMC | 0998-598-5601</li>
-                </ul>
+                    <h3>HOTLINES</h3>
+
+                    <ul className="hotlines">
+                        <li>Imus CDRRMO | (046) 472-2618 / 0939-912-0887</li>
+                        <li>Imus BFP | (046) 416-3032 / 0915-528-3256</li>
+                        <li>Imus PNP | (046) 471-2656</li>
+                        <li>Imus Hospital | (046) 419-8300</li>
+                        <li>Cavite PDRRMO | (046) 419-1652</li>
+                        <li>National Emergency | 911</li>
+                        <li>Red Cross PH | 143</li>
+                        <li>NDRRMC | 0998-598-5601</li>
+                    </ul>
                 </div>
 
             </div>
 
-            
             <div className="copyright">
-                Copyright © 2025 Official Website of Bayan Luma V. 
+                Copyright © 2025 Official Website of Bayan Luma V.
                 All Rights Reserved | Terms of Use and Privacy Policy
             </div>
-            </footer>
-        );
-        }
+        </footer>
+    );
+}
 
-        export default Footer;
+export default Footer;
