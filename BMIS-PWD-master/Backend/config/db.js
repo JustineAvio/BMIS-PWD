cea493de666af6157c43c6c1a25ec76b75651a4c
Justine Avio <justinejeckho.avio@cvsu.edu.ph>
2026-06-05 21:45:51 +0800

    database connection

--- BMIS-PWD-master/Backend/config/db.js
+++ BMIS-PWD-master/Backend/config/db.js
@@ -3,11 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const dbconnection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const URLDB2=`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const dbconnection = mysql.createPool(URLDB2);
 
 module.exports = dbconnection;
