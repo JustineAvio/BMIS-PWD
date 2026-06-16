03f3c8ac6eacc9ffa4263fe6af2b7ce9d367abe1
Justine Avio <justinejeckho.avio@cvsu.edu.ph>
2026-06-17 07:43:02 +0800

    Fixing validators

--- BMIS-PWD-master/Backend/validators/AuthValidator.js
+++ BMIS-PWD-master/Backend/validators/AuthValidator.js
@@ -1,11 +1,11 @@
 const Joi = require("joi");
 
 const loginSchema = Joi.object({
-    username: Joi.string().required().messages({
+    Username: Joi.string().required().messages({
         'string.empty': 'Username cannot be empty.',
         'any.required': 'Username is required to log in.'
     }),
-    password: Joi.string().required().messages({
+    Password: Joi.string().required().messages({
         'string.empty': 'Password cannot be empty.',
         'any.required': 'Password is required.'
     })
