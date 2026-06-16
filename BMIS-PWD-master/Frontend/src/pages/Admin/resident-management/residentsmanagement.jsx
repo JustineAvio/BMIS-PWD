aca23bd218ae657b5c5936f03a76ae83cd352384
Justine Avio <justinejeckho.avio@cvsu.edu.ph>
2026-06-17 03:08:03 +0800

    fixing import part 2

--- BMIS-PWD-master/Frontend/src/pages/Admin/resident-management/residentsmanagement.jsx
+++ BMIS-PWD-master/Frontend/src/pages/Admin/resident-management/residentsmanagement.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./residents.css";
 import axios from "axios";
-import ResidentDeleteModal from "../../../components/admincomponents/ResidentDeleteModal/ResidentDeleteModal.jsx";
+import ResidentDeleteModal from "../../../components/admincomponents/ResidentDeleteModal/residentdeletemodal.jsx";
 import { toast } from "react-toastify";
 
 export default function Residents() {
